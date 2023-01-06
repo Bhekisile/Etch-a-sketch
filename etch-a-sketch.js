a83a7b314c29cb1fdd6aa19b67f2ef10cfce38cd
Bhekisile <bhekisilejozi@gmail.com>
2023-01-06 17:46:27 +0200

    Add prompt function to js page

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,28 @@
 const container = document.getElementById('container');
 
 function makeGrid(rows, cols) {
-   
+   document.documentElement.style.setProperty("--columns-row", rows);
     for (i=0; i < (rows * cols); i++) {
         const cell = document.createElement('div');
         cell.setAttribute('class', 'grid-item');
         cell.innerText = (i+1);
         container.appendChild(cell);
-        container.setAttribute('style','grid-template-rows:(16, '+i+');');
-        container.setAttribute('style','grid-template-columns:(16, '+i+');');
+        container.style.gridTemplateRows = 'repeat(${rows}, 1fr)';
+        container.style.gridTemplateColumns = 'repeat(${cols}, 1fr)';
     }
+    return;
 }
-makeGrid(16, 16);
 
+function clickAlert() {
+    let num = prompt("Please enter a number:", 16);
+    if (num == null || num == "") {
+        container.innerHTML = "Invalid number!";
+    } else if (num >100) {
+        container.innerHTML = "A number must not exceed 100"
+    } else {
+        container.innerHTML = "";
+        makeGrid(num, num);
+    }
+    return;
+    }
+ 
